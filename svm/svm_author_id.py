e0ea739c63b32b4466ecaf40eac771f6583ab8a8
inim4 <iftitahu@gmail.com>
2015-03-23 13:00:07 +0700

    Update svm_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -28,5 +28,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 ### your code goes here ###
 
 #########################################################
+from sklearn.svm import SVC
+clf = SVC(kernel="rbf")
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
 
+def submitAccuracy():
+    return acc
